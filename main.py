ab0cb35dafb19ca9fb78ec631aa690823c3d7f1b
Jack Parr <jackparr143@gmail.com>
2023-01-23 12:36:10 +0000

    test change

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_new = np.linspace(0, 1, 100)
-y = np.exp(x_new)
+x = np.linspace(0, 1, 100)
+y = np.exp(x)
 
-plt.plot(x_new, y)
+plt.plot(x, y)
 plt.show()
