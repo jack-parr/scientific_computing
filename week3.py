57a729d7fbdd98f5a7fef6827a47ef54d00768a5
Jack Parr <jackparr143@gmail.com>
2023-02-14 17:18:11 +0000

    fixing orbit shooting

--- week3.py
+++ week3.py
@@ -22,8 +22,8 @@ x2_sol = [i[1] for i in x_sol]
 plt.plot(t_sol, x1_sol, t_sol, x2_sol)
 plt.legend(["x", "y"])
 
-lc_start = 667
-lc_end = lc_start + 199
+lc_start = 711
+lc_end = lc_start + 200
 print('LC START: x = ' + str(x1_sol[lc_start]) + ', y = ' + str(x2_sol[lc_start]))
 print('LC END: x = ' + str(x1_sol[lc_end]) + ', y = ' + str(x2_sol[lc_end]))
 print('PERIOD = ' + str(t_sol[lc_end] - t_sol[lc_start]))
@@ -34,10 +34,9 @@ def shooting_prob(func):
     def G(x0, args):
         T = x0[-1]
         x1 = x0[:-1]
-        print(x0)
         x_sol, t_sol = myfunc.solve_to(func, 'rk4', x1, 0, T, 0.01, args)
         phase_con = func(x1, 0, args)
-        G_vec = [x1[0] - x_sol[-1][0], x1[1] - x_sol[-1][1], phase_con[0]]
+        G_vec = [x1[0] - x_sol[-1][0], x1[1] - x_sol[-1][1], phase_con[1]]
         return G_vec
     return G
 
@@ -50,12 +49,18 @@ def orbit_shoot(func, x0, solver, args):
 a = 1
 b = 0.2
 d = 0.1
-lc = orbit_shoot(pred_prey, [0.3, 0.25, 35], scipy.optimize.fsolve, args=[a, b, d])
-print('---------------')
-print(lc)
-x_sol, t_sol = myfunc.solve_to(pred_prey, 'rk4', [0.3, 0.25], 0, 100, 0.1, args=[a, b, d])
+orbit = orbit_shoot(pred_prey, [0.6, 0.6, 20], scipy.optimize.fsolve, args=[a, b, d])
+
+x_sol, t_sol = myfunc.solve_to(pred_prey, 'rk4', [1, 1], 0, 100, 0.1, args=[a, b, d])
 x1_sol = [i[0] for i in x_sol]
 x2_sol = [i[1] for i in x_sol]
 plt.plot(x1_sol, x2_sol)
-plt.plot(lc[0], lc[1], 'ro')
-# %%
+plt.plot(orbit[0], orbit[1], 'ro')
+
+x_orb, t_orb = myfunc.solve_to(pred_prey, 'rk4', [orbit[0], orbit[1]], 0, orbit[2], 0.01, args=[a, b, d])
+x1_orb = [i[0] for i in x_orb]
+x2_orb = [i[1] for i in x_orb]
+plt.plot(x1_orb, x2_orb)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Predator Prey Model')
