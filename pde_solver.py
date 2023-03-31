865fdfffd7f4d0d80c0be19f3ef4ff80a66c6d6f
Jack Parr <jackparr143@gmail.com>
2023-03-31 12:39:23 +0100

    moving explicit euler into function

--- pde_solver.py
+++ pde_solver.py
@@ -4,9 +4,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 # %%
-def solve_heat(D, x_max, t_max, nx, nt):
-    # explicit euler
-    return 1
+def solve_heat(method, D, x_max, t_max, nx, nt):
+
+    dx = (x_max - x_min) / nx
+    dt = (t_max - t_min) / nt
+    # check stability condition
+    check = (dt * D) / (dx ** 2)
+    if check > 0.5:
+        raise Exception('Stability condition not met.')
+    
+    x_arr = np.linspace(x_min, x_max, nx+1)
+    t_arr = np.linspace(t_min, t_max, nt+1)
+    u_t = initial(x_arr[1:nx], 0, x_min, x_max)
+
+    # CREATE MATRICES
+    if method == 'explicit_euler':
+        A = np.zeros((nx-1, nx-1))
+        A[0, 0] = -2
+        A[0, 1] = 1
+        A[nx-2, nx-3] = 1
+        A[nx-2, nx-2] = -2
+        for i in range(1, nx-2):
+            A[i, i-1] = 1
+            A[i, i] = -2
+            A[i, i+1] = 1
+        
+        def make_b(t):
+            b = np.zeros(nx-1)
+            b[0] = l_bound(x_min, t)
+            b[-1] = r_bound(x_max, t)
+            return b
+    
+    # solvig using solve_ivp
+    def PDE(t, u, D, A, b):
+        return D / dx**2 * (A @ u + b)
+
+    b = make_b(0)
+    sol = sp.integrate.solve_ivp(PDE, (t_min, t_max), u_t, args=(D, A, b))
+    t = sol.t
+    u = sol.y
+    u_t = u[:,-1]
+    u_t = np.concatenate((np.array([l_bound(x_min, 0)]), u_t, np.array([r_bound(x_max, 0)])))
+
+    return u_t, x_arr
 # %%
 # paramters
 D = 1
@@ -27,6 +67,9 @@ def initial(x, t, x_min, x_max):
     y = np.sin((math.pi * x) / (x_max - x_min))
     return y
 
+u_t, x_arr = solve_heat('explicit_euler', D, x_max, t_max, nx, nt)
+
+# %%
 dx = (x_max - x_min) / nx
 dt = (t_max - t_min) / nt
 
