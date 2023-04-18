380fc2befeba234c640221205ac9e132c1144ee0
Jack Parr <jackparr143@gmail.com>
2023-04-18 15:17:08 +0100

    cleaning documentation

--- ode_solver.py
+++ ode_solver.py
@@ -59,20 +59,20 @@ def rk4_step(func, x0, t0, delta_t, args):
     return x0 + (k1/6 + k2/3 + k3/3 + k4/6)*delta_t, t1
 
 
-def solve_to(func, method, x1, t1, t2, deltat_max, args):
+def solve_to(func, method, x0, t0, t1, deltat_max, args):
     """
-    Solves the function from (x1, t1) to (x2, t2) taking steps no larger than 'deltat_max', using the method defined by 'method'.
+    Solves the function from (x0, t0) to (x1, t1) taking steps no larger than 'deltat_max', using the method defined by 'method'.
     ----------
     Parameters
     func : function
         The ODE to solve. The ODE function should be in first-order form, take a single list input and return the right-hand side of the ODE as a numpy.array. 
     method : string
         Either 'euler' for Euler method, or 'rk4' for 4th-Order Runge-Kutta method.
-    x1 : list
-        Initial values at 't1'.
-    t1 : float OR int
+    x0 : list
+        Initial values at 't0'.
+    t0 : float OR int
         Initial time value.
-    t2 : float OR int
+    t1 : float OR int
         Desired time value.
     deltat_max : float OR int
         Maximum allowed timestep.
@@ -88,15 +88,15 @@ def solve_to(func, method, x1, t1, t2, deltat_max, args):
     elif method == 'rk4':
         fstep = rk4_step
 
-    x_sol = np.empty(shape=(math.floor((t2-t1)/deltat_max)+2, len(x1)+1))*np.nan
-    x_sol[0][:len(x1)] = x1
-    x_sol[0][-1] = t1
+    x_sol = np.empty(shape=(math.floor((t1-t0)/deltat_max)+2, len(x0)+1))*np.nan
+    x_sol[0][:len(x0)] = x0
+    x_sol[0][-1] = t0
 
     i = 1
-    while not math.isclose(t1, t2):
-        x1, t1 = fstep(func, x1, t1, min([t2-t1, deltat_max]), args)
-        x_sol[i][:len(x1)] = x1
-        x_sol[i][-1] = t1
+    while not math.isclose(t0, t1):
+        x0, t0 = fstep(func, x0, t0, min([t1-t0, deltat_max]), args)
+        x_sol[i][:len(x0)] = x0
+        x_sol[i][-1] = t0
         i += 1
 
     return x_sol[~np.isnan(x_sol).any(axis=1), :].T
